e0507728c2ff9789c15f37ea991c4da802c3170e
aryanaman <amanaryankr@gmail.com>
2023-06-12 10:05:32 +0530

    added city model and create city api

---
+++ src/routes/v1/city-routes.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+const {CityController} = require("../../controllers")
+const {CityMiddlewares} = require("../../middlewares")
+
+router.post("/",
+    CityMiddlewares,
+    CityController.createCity)
+
+module.exports = router;
